30a312ba76ed34cd9f28a70d813b9fcb3a499619
wangjie <2578576255@qq.com>
2019-07-22 12:07:51 +0800

    '404页面'

---
+++ src/views/404/index.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="not-found">
+    <img src="../../assets/404.gif" alt="">
+</div>
+    
+</template>
+
+<script>
+export default {
+    name: 'not-found',
+    data(){
+        return{}
+    },
+    components: {},
+    methods: {},
+    
+}
+</script>
+
+<style scoped>
+.not-found{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+.not-found img{
+    width: 100%;
+    height: 100%;
+}
+</style>
